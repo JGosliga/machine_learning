1db4d1c6e7160425072446f3d5418d6fa44e16da
Julian Gosliga <julian_gosliga@hotmail.com>
2019-07-11 09:46:11 +0100

    Data loading into different file w/ validation set

---
+++ mnist_loader.py
@@ -0,0 +1,85 @@
+import struct as st
+import sys, os
+import numpy as np
+
+def import_data():
+    # Obtain the current directory for the python file
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+    filename = {'training_images' : 'train-images-idx3-ubyte' ,
+                'training_labels' : 'train-labels-idx1-ubyte',
+                'test_images'     : 't10k-images-idx3-ubyte',
+                'test_labels'     : 't10k-labels-idx1-ubyte'}
+
+    # Extract the test images
+    test_images_file = open(os.path.join(__location__, filename['test_images']),'rb')
+    test_images_file.seek(4)
+    # Find information regarding images
+    nImg_test = st.unpack('>I',test_images_file.read(4))[0] # Number of images
+    nR_test = st.unpack('>I',test_images_file.read(4))[0] # Number of rows
+    nC_test = st.unpack('>I',test_images_file.read(4))[0] # Numbe of columns
+    nBytesTotal = nImg_test*nR_test*nC_test*1 # Each pixel data is 1 byte
+    # Organise images into 784 pixel long row vectors
+    test_images_array = 1 - np.asarray(st.unpack('>'+'B'*nBytesTotal, 
+                                        test_images_file.read(nBytesTotal))).reshape((nImg_test,nR_test*nC_test)) / 255
+
+    # Extract the training labels
+    test_labels_file = open(os.path.join(__location__, filename['test_labels']),'rb')
+    # As before, skip the first 4 bytes
+    test_labels_file.seek(4)
+    nLbls_test = st.unpack('>I',test_labels_file.read(4))[0] # Number of labels
+    nBytesTotal = nLbls_test*1 # Each label is 1 byte
+    test_labels_array = np.array(st.unpack('>'+'B'*nBytesTotal,test_labels_file.read(nBytesTotal))).reshape(nLbls_test)
+
+    # Extract the training images
+    train_images_file = open(os.path.join(__location__, filename['training_images']),'rb')
+    # Skip first 4 bytes
+    train_images_file.seek(4)
+    # Find information regarding number and size of images
+    nImg_train = st.unpack('>I',train_images_file.read(4))[0] # Number of images
+    nR_train = st.unpack('>I',train_images_file.read(4))[0] # Number of rows
+    nC_train = st.unpack('>I',train_images_file.read(4))[0] # Numbe of columns
+    nBytesTotal = nImg_train*nR_train*nC_train*1 # Each pixel data is 1 byte
+    # Organise images into 784 pixel long row vectors
+    train_images_array = 1 - np.asarray(st.unpack('>'+'B'*nBytesTotal,
+                                            train_images_file.read(nBytesTotal))).reshape((nImg_train,nR_train*nC_train)) / 255
+
+    # Extract the training labels
+    train_labels_file = open(os.path.join(__location__, filename['training_labels']),'rb')
+    # Skip first 4 bytes again
+    train_labels_file.seek(4)
+    nLbls_train = st.unpack('>I',train_labels_file.read(4))[0] # Number of labels
+    nBytesTotal = nLbls_train*1 # Each label is 1 byte
+    train_labels_array = np.array(st.unpack('>'+'B'*nBytesTotal,
+                                    train_labels_file.read(nBytesTotal))).reshape(nLbls_train)
+
+    validation_set_size = 10000
+
+    return zip_data(train_images_array[:-validation_set_size], train_labels_array[:-validation_set_size], 
+                    train_images_array[-validation_set_size:], train_labels_array[-validation_set_size:],
+                    test_images_array, test_labels_array)
+
+def zip_data(train_images_array, train_labels_array,
+             validation_images_array, validation_labels_array, 
+             test_images_array, test_labels_array):
+    # Create zip objects containing training images and vectorised inputs based on training labels
+    training_inputs = [np.reshape(x, (784, 1)) for x in train_images_array]
+    training_results = [label_to_vector(y) for y in train_labels_array]
+    training_data = zip(training_inputs, training_results)
+    # Create validation data containing validation images and regular labels
+    validation_inputs = [np.reshape(x, (784, 1)) for x in train_images_array]
+    validation_data = zip(validation_inputs, validation_labels_array)
+    # Create test data containing images and regular labels
+    test_inputs = [np.reshape(x, (784, 1)) for x in test_images_array]
+    test_data = zip(test_inputs, test_labels_array)
+
+    return training_data, validation_data, test_data
+    
+def label_to_vector(label):
+    # Set classification vector according to the labels
+    Y = np.zeros((10,1))
+    Y[label] = 1
+    return Y
+
+if __name__ == "__main__":
+    training_data, validation_data, test_data = import_data()
--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,6 +1,4 @@
 import numpy as np
-import struct as st
-import sys, os
 
 def sigmoid(Z):
     '''Sigmoid function'''
@@ -26,77 +24,6 @@ def vectorized_result(j):
     e = np.zeros((10, 1))
     e[j] = 1.0
     return e
-
-def import_data():
-    # Obtain the current directory for the python file
-    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
-    filename = {'training_images' : 'train-images-idx3-ubyte' ,
-                'training_labels' : 'train-labels-idx1-ubyte',
-                'test_images'     : 't10k-images-idx3-ubyte',
-                'test_labels'     : 't10k-labels-idx1-ubyte'}
-
-    # Extract the training images
-    train_images_file = open(os.path.join(__location__, filename['training_images']),'rb')
-    # Skip first 4 bytes
-    train_images_file.seek(4)
-    # Find information regarding number and size of images
-    nImg = st.unpack('>I',train_images_file.read(4))[0] # Number of images
-    nR = st.unpack('>I',train_images_file.read(4))[0] # Number of rows
-    nC = st.unpack('>I',train_images_file.read(4))[0] # Numbe of columns
-    nBytesTotal = nImg*nR*nC*1 # Each pixel data is 1 byte
-    # Organise images into 784 pixel long row vectors
-    train_images_array = 1 - np.asarray(st.unpack('>'+'B'*nBytesTotal,
-                                            train_images_file.read(nBytesTotal))).reshape((nImg,nR*nC)) / 255
-
-    # Extract the training labels
-    train_labels_file = open(os.path.join(__location__, filename['training_labels']),'rb')
-    # Skip first 4 bytes again
-    train_labels_file.seek(4)
-    nLbls = st.unpack('>I',train_labels_file.read(4))[0] # Number of labels
-    nBytesTotal = nLbls*1 # Each label is 1 byte
-    train_labels_array = np.array(st.unpack('>'+'B'*nBytesTotal,
-                                    train_labels_file.read(nBytesTotal))).reshape(nLbls)
-
-    # Extract the test images
-    test_images_file = open(os.path.join(__location__, filename['test_images']),'rb')
-    test_images_file.seek(4)
-    # Find information regarding images
-    nImg = st.unpack('>I',test_images_file.read(4))[0] # Number of images
-    nR = st.unpack('>I',test_images_file.read(4))[0] # Number of rows
-    nC = st.unpack('>I',test_images_file.read(4))[0] # Numbe of columns
-    nBytesTotal = nImg*nR*nC*1 # Each pixel data is 1 byte
-    # Organise images into 784 pixel long row vectors
-    test_images_array = 1 - np.asarray(st.unpack('>'+'B'*nBytesTotal, 
-                                        test_images_file.read(nBytesTotal))).reshape((nImg,nR*nC)) / 255
-
-    # Extract the training labels
-    test_labels_file = open(os.path.join(__location__, filename['test_labels']),'rb')
-    # As before, skip the first 4 bytes
-    test_labels_file.seek(4)
-    nLbls = st.unpack('>I',test_labels_file.read(4))[0] # Number of labels
-    nBytesTotal = nLbls*1 # Each label is 1 byte
-    test_labels_array = np.array(st.unpack('>'+'B'*nBytesTotal,test_labels_file.read(nBytesTotal))).reshape(nLbls)
-
-    return zip_data(train_images_array, train_labels_array, test_images_array, test_labels_array)
-
-def zip_data(train_images_array, train_labels_array, test_images_array, test_labels_array):
-    training_inputs = [np.reshape(x, (784, 1)) for x in train_images_array]
-    training_results = [vectorized_result(y) for y in train_labels_array]
-    training_data = zip(training_inputs, training_results)
-    test_inputs = [np.reshape(x, (784, 1)) for x in test_images_array]
-    test_data = zip(test_inputs, test_labels_array)
-
-    return training_data, test_data
-    
-def convert_labels_to_input(labels):
-    # Set classification vector according to the labels
-    Y = np.zeros((labels.shape[0],10,1))
-    for idx, y in enumerate(Y):
-        clss = labels[idx]
-        # Set the corresponding position in output vector to reflect class label
-        y[clss] = 1
-    return Y
 
 class NeuralNetwork:
     def __init__(self, architecture, seed = 1):
