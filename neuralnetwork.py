7f300abd613f9b710519614178fdabbc7aa923ca
Julian Gosliga <julian_gosliga@hotmail.com>
2019-07-12 14:36:34 +0100

    Changed activation functions

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -168,4 +168,4 @@ if __name__ == "__main__":
     training_data, validation_data, test_data = mnist_loader.import_data()
     layers = [[784, "input"], [32, "sigmoid"], [32, "relu"], [10, "sigmoid"]]
     net = NeuralNetwork(layers)
-    net.train_network(training_data, epochs=10, batch_size=10, eta=1, test_data=test_data)
+    net.train_network(training_data, epochs=10, batch_size=50, eta=2, test_data=test_data)
